3d436665667e138419f0fa1345b0a5e770c5a0b4
Christian Le <cle1994@users.noreply.github.com>
2015-08-16 22:07:04 -0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,16 @@ var app = angular.module('appName', [
         'ui.router',
         'ngTouch'
     ]);
+    
+app.run([
+    '$rootScope',
+    '$state',
+    '$stateParams',
+    function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+    }
+]);
 
 app.config(['$stateProvider',
     '$urlRouterProvider',
